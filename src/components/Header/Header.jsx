3fb9bba04b76abf52ff683442a6e6206bd59eb15
Krilzs <jerofc143@gmail.com>
2024-09-04 22:37:26 -0300

    Created Button for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+export function Header() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <h1>
+        <span>JM</span>board
+      </h1>
+      <ul className="menu">
+        <li>Boards Recientes</li>
+      </ul>
+      <button className="create-button">Crear</button>
+    </header>
+  );
+}
