aa3b830669d3e5485ec38e7c789fa26cf82faa25
Matias Lascurain <matiaslascurain04@gmail.com>
2024-09-11 15:10:59 -0300

    trying to fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-react-deploy/",
+  base: "https://jmboard.vercel.app/",
   plugins: [react()],
 });
